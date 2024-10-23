07cd15522d6bade23f95939a2117943e828b3d2b
Fabian Schillig <fabian@libf.de>
2024-10-23 14:02:02 +0200

    Add ARM32 target

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,12 +13,14 @@ repositories {
 kotlin {
     val hostOs = System.getProperty("os.name")
     val isArm64 = System.getProperty("os.arch") == "aarch64"
+    val isArm32 = System.getProperty("os.arch") == "arm"
     val isMingwX64 = hostOs.startsWith("Windows")
     val nativeTarget = when {
         hostOs == "Mac OS X" && isArm64 -> macosArm64("native")
         hostOs == "Mac OS X" && !isArm64 -> macosX64("native")
         hostOs == "Linux" && isArm64 -> linuxArm64("native")
-        hostOs == "Linux" && !isArm64 -> linuxX64("native")
+        hostOs == "Linux" && isArm32 -> linuxArm32Hfp("native")
+        hostOs == "Linux" && !isArm64 && !isArm32 -> linuxX64("native")
         isMingwX64 -> mingwX64("native")
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
